bd9b8f57a3cc17cf8ee618bc608af225cf36d2d1
vinod <mechvnod@gmail.com>
2025-04-19 22:20:13 +0530

    terraform-aws-ec2-module

---
+++ ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "vinod_ec2" {
+    ami = var.ami_id
+    vpc_security_group_ids = var.sg_id
+    instance_type = local.instance_type
+    tags = var.tags
+
+    
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "ami _id" {
+    default = "ami-09c813fb71547fc4f"
+}
+
+# mandatory
+variable "sg_id" {
+
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+    validation {
+        condition = contains(["t3.micro, t3.small, t3.medium"], var.instance_type)
+        error_message = "valid value for instance_type are: t3.micro, t3.small, t3.medium"
+    }
+}
+
+# optional
+variable "tags" {
+    default = {}
+}
